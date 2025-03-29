78be5c90e7d31391aaaf586e837f3f0f5773849b
Amine <ma.saidane@esi-sba.dz>
2025-03-29 06:22:43 +0100

    imagine maining nilah

--- database/migrations/2025_03_19_220352_teacher.php
+++ database/migrations/2025_03_19_220352_teacher.php
@@ -6,22 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         schema::create('teachers',function(Blueprint $table){
             $table->id();
             $table->string('fullName');
-            $table->json('grades');
+            $table->json('grades'); //update later (either split into another table or figure out something else)
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         schema::dropIfExists('teachers');
